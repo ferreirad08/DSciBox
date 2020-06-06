c05694a0fb980153ac026e8ec7a26b3be1b8783f
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-06-06 17:04:02 -0400

    Update binning.m

--- binning.m
+++ binning.m
@@ -10,11 +10,13 @@ function xt = binning(x,n_bins)
 %Description
 %1. Discretize the data of a vector based on quantiles.
 %
+%n_bins is a number of groupings.
+%
 %Examples
 %1.
 %     x = [30, 64, 49, 26, 69, 23, 56, 7, 69, 67,...
 %         87, 14, 67, 33, 88, 77, 75, 47, 44, 93];
-%     n_bins = 10; % number of groupings
+%     n_bins = 10;
 %     xt = binning(x,n_bins)
 %     xt =
 %         2 5 4 1 7 1 4 0 7 6 8 0 6 2 9 8 7 3 3 9
