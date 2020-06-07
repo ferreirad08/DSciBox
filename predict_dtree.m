2c3607859458476822db2905e4d681c984d08e33
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-06-07 15:39:16 -0400

    Add files via upload

--- predict_dtree.m
+++ predict_dtree.m
@@ -1,33 +1,40 @@
-function label = predict_dtree(X,Y,Xnew)  
-    while 1
-        g = gain(X,Y);
-        [~,I] = max(g);
-        [str,~,values] = unique(X(:,I));
-        [~,value] = ismember(Xnew(I),str);
-        X = X(values==value,:);
-        X(:,I) = [];
-        Y = Y(values==value);
-        Xnew(I) = [];
+function label = predict_dtree(X,Y,Xnew)
+if isnumeric(X) && isnumeric(Xnew)
+    Xt = binning([X; Xnew],3);
+    n = size(X,1);
+    X = Xt(1:n,:);
+    Xnew = Xt(n+1:end,:);
+end
 
-        C = unique(Y);
-        if numel(C)==1
-            label = C;
-            break
-        end
+while 1
+    g = gain(X,Y);
+    [~,I] = max(g);
+    [str,~,values] = unique(X(:,I));
+    [~,value] = ismember(Xnew(I),str);
+
+    Y = Y(values==value);
+    C = unique(Y);
+    if numel(C)==1
+        label = C;
+        break
     end
+
+    X = X(values==value,:); X(:,I) = [];
+    Xnew(I) = [];
+end
 end
 
 function g = gain(X,Y)
-    [n_samples,n_features] = size(X);
-    [~,~,Y] = unique(Y);
-    g = ones(1,n_features)*entropy(Y);
-    for i = 1:n_features
-        feature = X(:,i);
-        [~,~,feature] = unique(feature);
-        for j=1:max(feature)
-            p = histc(feature(feature==j),j)/n_samples;
-            h = entropy(Y(feature==j));
-            g(i) = g(i) - p*h;
-        end
+[n_samples,n_features] = size(X);
+[~,~,Y] = unique(Y);
+g = ones(1,n_features)*entropy(Y);
+for i = 1:n_features
+    feature = X(:,i);
+    [~,~,feature] = unique(feature);
+    for j=1:max(feature)
+        p = histc(feature(feature==j),j)/n_samples;
+        h = entropy(Y(feature==j));
+        g(i) = g(i) - p*h;
     end
+end
 end
