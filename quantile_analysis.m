0af514736044312582de3a21276d02f5eec5e05f
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-06-06 22:06:39 -0400

    Update and rename quantile.m to quantile_analysis.m

--- quantile_analysis.m
+++ quantile_analysis.m
@@ -1,11 +1,11 @@
-function Q = quantile(X,p)
+function Q = quantile_analysis(X,p)
 %Author: David Ferreira - Federal University of Amazonas
 %Contact: ferreirad08@gmail.com
 %
-%quantile
+%Quantile Analysis
 %
 %Syntax
-%1. Q = quantile(X,p)
+%1. Q = quantile_analysis(X,p)
 %
 %Description 
 %1. Calculate the quantiles of a vector or matrix data based on linear regression.
@@ -14,7 +14,7 @@ function Q = quantile(X,p)
 %1.
 %     v = [10 1 2 3 4 7];
 %     p = [0.25 0.50 0.75];
-%     Q = quantile(v,p)
+%     Q = quantile_analysis(v,p)
 %     Q = 
 %         2.2500
 %         3.5000
@@ -22,7 +22,7 @@ function Q = quantile(X,p)
 %
 %2.
 %     X = [1 2; 2 5; 3 6; 4 10; 7 11; 10 13];
-%     Q = quantile(X,p)
+%     Q = quantile_analysis(X,p)
 %     Q = 
 %         2.2500    5.2500
 %         3.5000    8.0000
