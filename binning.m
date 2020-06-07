2e7f373aab22df81f40ad43bd16faeb22b68a478
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-06-07 10:17:04 -0400

    Update and rename quantile_binning.m to binning.m

--- binning.m
+++ binning.m
@@ -1,11 +1,11 @@
-function xt = quantile_binning(x,n_bins)
+function xt = binning(x,n_bins)
 %Author: David Ferreira - Federal University of Amazonas
 %Contact: ferreirad08@gmail.com
 %
 %Quantile Binning Transformation
 %
 %Syntax
-%1. xt = quantile_binning(x,n_bins)
+%1. xt = binning(x,n_bins)
 %
 %Description
 %1. Discretize the data of a vector based on quantiles.
@@ -18,13 +18,13 @@ function xt = quantile_binning(x,n_bins)
 %     x = [30, 64, 49, 26, 69, 23, 56, 7, 69, 67,...
 %         87, 14, 67, 33, 88, 77, 75, 47, 44, 93];
 %     n_bins = 10;
-%     xt = quantile_binning(x,n_bins)
+%     xt = binning(x,n_bins)
 %     xt =
 %         2 5 4 1 7 1 4 0 7 6 8 0 6 2 9 8 7 3 3 9
 
 if ~isrow(x), x = x'; end
 
 p = (1:n_bins-1)/n_bins;
-Q = quantile_analysis(x,p);
+Q = quantile(x,p);
 xt = sum(repmat(x,numel(Q),1)>=repmat(Q,1,(numel(x))));
 end
