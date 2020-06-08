85922c2ad5668d30a58a32e8b0cb87a78c28b4dd
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-06-08 00:55:41 -0400

    Add files via upload

---
+++ examples.m
@@ -0,0 +1,21 @@
+load('16x220samples.mat')
+X = samples(:,1:8); Y = samples(:,9);
+
+load('fisheriris.mat')
+X = meas; Y = species;
+
+[X,Xnew,Y,Ynew] = sampling(X,Y,0.25);
+
+label = predict_knneighbors(X,Y,Xnew,5);
+accuracy = accuracy_score(Ynew,label)
+
+label = predict_gaussiannb(X,Y,Xnew);
+accuracy = accuracy_score(Ynew,label)
+
+load('golf-dataset.mat')
+X = predictors; Y = target;
+
+[X,Xnew,Y,Ynew] = sampling(X,Y,0.05);
+
+label = predict_dtree(X,Y,Xnew);
+accuracy = accuracy_score(Ynew,label)
