7b1d2f3df9938a4aedf286e32066c10ab9bb2cfd
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-06-08 00:26:26 -0400

    Add files via upload

---
+++ sampling.m
@@ -0,0 +1,9 @@
+function [X,Xnew,Y,Ynew] = sampling(X,Y,p)
+    % Simple or stratified random sampling.
+    n = numel(Y);
+    i = randperm(n,round(n*p));
+    Xnew = X(i,:);
+    X(i,:) = [];
+    Ynew = Y(i);
+    Y(i) = [];
+end
