e6e93ef7918b9abe7c56b88a331d3d71fc17ecef
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-06-08 09:34:25 -0400

    Update predict_dtree.m

--- predict_dtree.m
+++ predict_dtree.m
@@ -31,12 +31,10 @@ function g = gain(X,Y)
 [~,~,Y] = unique(Y);
 g = ones(1,n_features)*entropy(Y);
 for i = 1:n_features
-    feature = X(:,i);
-    [~,~,feature] = unique(feature);
+    [~,~,feature] = unique(X(:,i));
     for j = 1:max(feature)
         p = histc(feature(feature==j),j)/n_samples;
-        e = entropy(Y(feature==j));
-        g(i) = g(i) - p*e;
+        g(i) = g(i) - p*entropy(Y(feature==j));
     end
 end
 end
