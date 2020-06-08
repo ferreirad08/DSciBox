7da6b2cf27fbd7237493982000292700d2bad734
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-06-08 09:43:26 -0400

    Update examples.m

--- examples.m
+++ examples.m
@@ -4,7 +4,7 @@ X = samples(:,1:8); Y = samples(:,9);
 load('fisheriris.mat')
 X = meas; Y = species;
 
-[X,Xnew,Y,Ynew] = sampling(X,Y,0.25);
+[X,Xnew,Y,Ynew] = data_sampling(X,Y,0.25);
 
 label = predict_knneighbors(X,Y,Xnew,5);
 accuracy = accuracy_score(Ynew,label)
@@ -15,7 +15,7 @@ accuracy = accuracy_score(Ynew,label)
 load('golf-dataset.mat')
 X = predictors; Y = target;
 
-[X,Xnew,Y,Ynew] = sampling(X,Y,0.05);
+[X,Xnew,Y,Ynew] = data_sampling(X,Y,0.05);
 
 label = predict_dtree(X,Y,Xnew);
 accuracy = accuracy_score(Ynew,label)
