c487c6b625dc4fcf43d468a68de57cd264e8ffbf
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-06-08 23:21:36 -0400

    Update predict_dtree.m

--- predict_dtree.m
+++ predict_dtree.m
@@ -2,9 +2,8 @@ function label = predict_dtree(X,Y,Xnew)
 if isnumeric(X) && isnumeric(Xnew)
     n = size(X,1);
     n_bins = round(n^(1/3)*2); % Regra de Rice
-    Xt = binning([X; Xnew],n_bins);
-    X = Xt(1:n,:);
-    Xnew = Xt(n+1:end,:);
+    [X,Q] = binning(X,n_bins);
+    Xnew = binning(Xnew,Q);
 end
 
 [C,~,Y] = unique(Y);
@@ -38,7 +37,8 @@ g = gain(X,Y);
 [str,~,values] = unique(X(:,I));
 [~,value] = ismember(Xnew(I),str);
 
-X = X(values==value,:); X(:,I) = [];
+X = X(values==value,:);
+X(:,I) = [];
 Y = Y(values==value);
 Xnew(I) = [];
 end
