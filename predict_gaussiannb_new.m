956f3b73d0355d696a2a18a7ac771ae1807a1654
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-06-09 22:10:57 -0400

    Add files via upload

---
+++ predict_gaussiannb_new.m
@@ -0,0 +1,57 @@
+function label = predict_gaussiannb_new(X,Y,Xnew)
+%Author: David Ferreira - Federal University of Amazonas
+%Contact: ferreirad08@gmail.com
+%
+%Gaussian Naive Bayes (GNB)
+%
+%Syntax
+%1. label = predict_gaussiannb(X,Y,Xnew)
+%
+%Description 
+%1. Returns the estimated labels of one or multiple test instances.
+%
+%X is a M-by-N matrix, with M instances of N features. 
+%Y is a M-by-1 matrix, with respective M labels to each training instance. 
+%Xnew is a P-by-N matrix, with P instances of N features to be classified.
+%
+%Examples
+%1.
+%     load fisheriris
+%     X = meas;
+%     Y = species;
+%     Xnew = [min(meas);mean(meas);max(meas)];
+%     label = predict_gaussiannb(X,Y,Xnew)
+%     label = 
+%         'setosa'
+%         'versicolor'
+%         'virginica'
+
+[C,~,Y] = unique(Y);
+n_class = numel(C);
+n_features = size(X,2);
+S = zeros(n_class,n_features);
+M = zeros(n_class,n_features);
+
+for i = 1:n_class
+    A = X(Y==i,:);
+    M(i,:) = mean(A);
+    S(i,:) = std(A,1);
+end
+
+% Class prior probability
+prior = histc(Y,1:n_class)/numel(Y);
+
+n_tests = size(Xnew,1);
+label = zeros(n_tests,1);
+for i = 1:n_tests
+    % Probability density function (PDF) of the normal distribution
+    gauss = 1./(S.*sqrt(2.*pi))...
+        .*exp(-1/2.*((repmat(Xnew(i,:),n_class,1)-M)./S).^2);
+    % Product
+    probability = prod([gauss prior],2);
+    % Sort the probabilities in descending order and check the estimated label
+    [~,label(i)] = max(probability);
+end
+
+label = C(label);
+end
