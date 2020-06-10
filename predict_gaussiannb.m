e9bae7c24fda15b41012a383980422c645cf0cec
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-06-10 19:42:32 -0400

    Update predict_gaussiannb.m

--- predict_gaussiannb.m
+++ predict_gaussiannb.m
@@ -1,5 +1,7 @@
 function label = predict_gaussiannb(X,Y,Xnew)
-%Author: David Ferreira - Federal University of Amazonas
+%Author: David Ferreira
+%PhD student in Electrical Engineering
+%Federal University of Amazonas
 %Contact: ferreirad08@gmail.com
 %
 %Gaussian Naive Bayes (GNB)
