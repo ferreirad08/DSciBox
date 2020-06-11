0c534f6e07f610a9aa53e334caf202f5f69749a2
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-06-11 16:28:16 -0400

    Add files via upload

--- predict_gaussiannb.m
+++ predict_gaussiannb.m
@@ -1,4 +1,4 @@
-function label = predict_gaussiannb(X,Y,Xnew)
+function [label,model] = predict_gaussiannb(arg1,arg2,arg3)
 %Author: David Ferreira
 %PhD student in Electrical Engineering
 %Federal University of Amazonas
@@ -22,26 +22,50 @@ function label = predict_gaussiannb(X,Y,Xnew)
 %     load fisheriris
 %     X = meas;
 %     Y = species;
-%     Xnew = [min(meas);mean(meas);max(meas)];
-%     label = predict_gaussiannb(X,Y,Xnew)
-%     label = 
+%     Xnew = [min(meas);max(meas)];
+%     [label,model] = predict_gaussiannb(X,Y,Xnew)
+%     label =
 %         'setosa'
-%         'versicolor'
 %         'virginica'
+%     model = 
+%              C               M               S           prior 
+%         ____________    ____________    ____________    _______
+% 
+%         'setosa'        [1x4 double]    [1x4 double]    0.33333
+%         'versicolor'    [1x4 double]    [1x4 double]    0.33333
+%         'virginica'     [1x4 double]    [1x4 double]    0.33333
+%
+%2.
+%     Xnew = [mean(meas)];
+%     label = predict_gaussiannb(model,Xnew)
+%     label = 
+%         'versicolor'
 
-[C,~,Y] = unique(Y);
-n_class = numel(C);
+% Check the number of input
+if nargin > 2
+    X = arg1; 
+    Y = arg2;
+    Xnew = arg3;    
+    [C,~,Y] = unique(Y);
+    n_class = numel(C);
 
-% Calculate the means and standard deviations
-M = zeros(n_class,size(X,2)); S = M;
-for i = 1:n_class
-    A = X(Y==i,:);
-    M(i,:) = mean(A);
-    S(i,:) = std(A,1);
+    % Calculate the means and standard deviations
+    M = zeros(n_class,size(X,2)); S = M;
+    for i = 1:n_class
+        A = X(Y==i,:);
+        M(i,:) = mean(A);
+        S(i,:) = std(A,1);
+    end
+
+    % Class prior probability
+    prior = histc(Y,1:n_class)/numel(Y);
+    % Classification model
+    model = table(C,M,S,prior);
+else
+    model = arg1;
+    Xnew = arg2;
+    n_class = numel(model.C);
 end
-
-% Class prior probability
-prior = histc(Y,1:n_class)/numel(Y);
 
 n_tests = size(Xnew,1);
 label = zeros(n_tests,1);
@@ -49,12 +73,12 @@ for i = 1:n_tests
     % Repeats measurements in a matrix
     meas = repmat(Xnew(i,:),n_class,1);
     % Probability density function (PDF) of the normal distribution
-    gauss = 1./(S.*sqrt(2.*pi)).*exp(-1/2.*((meas-M)./S).^2);
+    gauss = 1./(model.S.*sqrt(2.*pi)).*exp(-1/2.*((meas-model.M)./model.S).^2);
     % Product
-    probability = prod([gauss prior],2);
+    probability = prod([gauss model.prior],2);
     % Check the highest probability and the respective label
     [~,label(i)] = max(probability);
 end
 
-label = C(label);
+label = model.C(label);
 end
