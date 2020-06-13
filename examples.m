dc9a333e0e570e333abd67f59a5b1d2432bb2601
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-06-13 14:33:18 -0400

    Add files via upload

--- examples.m
+++ examples.m
@@ -1,7 +1,6 @@
-clear
 clc
 
-disp('Fisher iris test')
+disp('Fisher iris')
 load('fisheriris.mat')
 [X,Xnew,Y,Ynew] = data_sampling(meas,species,0.25);
 
@@ -17,23 +16,7 @@ disp(['DT: ' num2str(accuracy_score(Ynew,label))])
 %%
 
 disp(' ')
-disp('RSSI indoor test')
-load('RSSI-indoor.mat')
-[X,Xnew,Y,Ynew] = data_sampling(access_points,references_points,0.25);
-
-label = predict_knneighbors(X,Y,Xnew,5);
-disp(['kNN: ' num2str(accuracy_score(Ynew,label))])
-
-label = predict_gaussiannb(X,Y,Xnew);
-disp(['GNB: ' num2str(accuracy_score(Ynew,label))])
-
-label = predict_dtree(X,Y,Xnew);
-disp(['DT: ' num2str(accuracy_score(Ynew,label))])
-
-%%
-
-disp(' ')
-disp('Golf dataset test')
+disp('Golf dataset')
 load('golf-dataset.mat')
 [X,Xnew,Y,Ynew] = data_sampling(predictors,target,0.25);
 
