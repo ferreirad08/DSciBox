7c67d312d509f558f2619a479fb53b9823132222
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-06-13 13:13:40 -0400

    Update predict_dtree.m

--- predict_dtree.m
+++ predict_dtree.m
@@ -29,7 +29,8 @@ function label = predict_dtree(X,Y,Xnew)
 
 % Discretize, if variables are continuous
 if isnumeric(X) && isnumeric(Xnew)
-    n_bins = round(numel(Y)^(1/3)*2); % Regra de Rice
+    % Compute the number of bins based on Rice's Rule
+    n_bins = round(numel(Y)^(1/3)*2);
     [X,Q] = binning(X,n_bins);
     Xnew = binning(Xnew,Q);
 end
