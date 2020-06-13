f1a34b3c552e3b0678c68dcc27e6c8feb2ee57ba
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-06-13 16:19:47 -0400

    Update principal_component.m

--- principal_component.m
+++ principal_component.m
@@ -9,7 +9,7 @@ Xcentered = X - repmat(M,size(X,1),1);
 V = cov(Xcentered);
 % Eigendecomposition of covariance matrix
 [vectors,values] = eig(V);
-% Sort eigenvalues ​​and associated eigenvectors
+% Sorts eigenvalues ​​and associated eigenvectors
 [~,i] = sort(sum(values),'descend');
 % Project data
 P = (vectors(:,i)'*Xcentered')';
