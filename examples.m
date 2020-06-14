898ef9240b82aafffc739459d87c5d02e15b4108
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-06-14 11:32:44 -0400

    Update examples.m

--- examples.m
+++ examples.m
@@ -2,7 +2,7 @@ clc
 
 disp('Fisher iris')
 load('fisheriris.mat')
-[X,Xnew,Y,Ynew] = data_sampling(meas,species,0.25);
+[X,Xnew,Y,Ynew] = data_sampling(meas,species,0.25,'stratified');
 
 label = predict_knneighbors(X,Y,Xnew,5);
 disp(['kNN: ' num2str(accuracy_score(Ynew,label))])
@@ -18,7 +18,7 @@ disp(['DT: ' num2str(accuracy_score(Ynew,label))])
 disp(' ')
 disp('Golf dataset')
 load('golf-dataset.mat')
-[X,Xnew,Y,Ynew] = data_sampling(predictors,target,0.25);
+[X,Xnew,Y,Ynew] = data_sampling(predictors,target,0.25,'stratified');
 
 label = predict_dtree(X,Y,Xnew);
 disp(['DT: ' num2str(accuracy_score(Ynew,label))])
