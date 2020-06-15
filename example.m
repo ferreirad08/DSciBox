f6390e4377472fc17a89063f98b2787b12ff67f7
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-06-15 18:57:36 -0400

    Add files via upload

--- example.m
+++ example.m
@@ -10,3 +10,16 @@ n_components = 2;
 pca = PCA(n_components);
 pca = pca.fit(X);
 Xt = pca.transform(X)
+
+% ----------------------
+
+X = [16     2;
+      5    11;
+      9     7;
+      4    14];
+
+n_bins = 3;
+
+b = Binning(n_bins);
+b = b.fit(X);
+Xt = b.transform(X)
