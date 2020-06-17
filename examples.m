cd4e6baac4dcb6ec2e27241055325879993ab7b3
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-06-17 12:47:20 -0400

    Add files via upload

--- examples.m
+++ examples.m
@@ -1,39 +1,40 @@
 clc
 
 % Add library folders to the search path
-addpath('datasets','dscibox_src')
+addpath('datasets',...
+        'dscibox_src')
 % savepath
 
 disp('Fisher iris')
 load('fisheriris.mat')
-[X,Xnew,Y,Ynew] = data_sampling(meas,species,0.30,'stratified');
+[X,Xnew,Y,Ynew] = dsb_utilities.data_sampling(meas,species,0.30,'stratified');
 
 k = 5; % k must be an integer, 5 is the default
-mk = kNNeighbors(k);
+mk = dsb_predictors.kNNeighbors(k);
 mk = mk.fit(X,Y);
 label = mk.predict(Xnew);
-disp(['kNN: ' num2str(accuracy_score(Ynew,label))])
+disp(['kNN: ' num2str(dsb_utilities.accuracy_score(Ynew,label))])
 % [Xnearest,Ynearest,distances] = mk.find(Xnew(2,:))
 
-mg = GaussianNB();
+mg = dsb_predictors.GaussianNB();
 mg = mg.fit(X,Y);
 label = mg.predict(Xnew);
-disp(['GNB: ' num2str(accuracy_score(Ynew,label))])
+disp(['GNB: ' num2str(dsb_utilities.accuracy_score(Ynew,label))])
 % [labels,probabilities] = mg.find(Xnew(2,:))
 
-mdt = DTree();
+mdt = dsb_predictors.DTree();
 mdt = mdt.fit(X,Y);
 label = mdt.predict(Xnew);
-disp(['DT: ' num2str(accuracy_score(Ynew,label))])
+disp(['DT: ' num2str(dsb_utilities.accuracy_score(Ynew,label))])
 
 %%
 
 disp(' ')
 disp('Golf dataset')
 load('golf-dataset.mat')
-[X,Xnew,Y,Ynew] = data_sampling(predictors,target,0.30,'stratified');
+[X,Xnew,Y,Ynew] = dsb_utilities.data_sampling(predictors,target,0.30,'stratified');
 
-mdt2 = DTree();
+mdt2 = dsb_predictors.DTree();
 mdt2 = mdt2.fit(X,Y);
 label = mdt2.predict(Xnew);
-disp(['DT: ' num2str(accuracy_score(Ynew,label))])
+disp(['DT: ' num2str(dsb_utilities.accuracy_score(Ynew,label))])
