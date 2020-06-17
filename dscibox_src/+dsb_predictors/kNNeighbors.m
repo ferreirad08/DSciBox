bfe35da3c6fd00343b39ed66a08fbdfdf7d5bb05
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-06-17 13:22:19 -0400

    Update kNNeighbors.m

--- dscibox_src/+dsb_predictors/kNNeighbors.m
+++ dscibox_src/+dsb_predictors/kNNeighbors.m
@@ -1,49 +1,79 @@
 classdef kNNeighbors
-    properties
-        k = 5
-        C
-        X
-        Y
-    end
-    methods
-        function obj = kNNeighbors(k)
-            if nargin > 0
-                obj.k = k;
-            end
-        end
-        function obj = fit(obj,X,Y)
-            [obj.C,~,obj.Y] = unique(Y);
-            obj.X = X;
-        end
-        function Ypred = predict(obj,Xnew)
-            P = size(Xnew,1);
-            Ypred = zeros(P,1);
-            for i = 1:P
-                % Euclidean distance between two points
-                A = repmat(Xnew(i,:),size(obj.X,1),1);
-                distances = sqrt(sum((A-obj.X).^2,2));
-                % Sort the distances in ascending order and check the k nearest training labels
-                [~,I] = sort(distances);
-                Ynearest = obj.Y(I(1:obj.k));
-                % Frequencies of the k nearest training labels
-                N = histc(Ynearest,1:max(Ynearest));
-                frequencies = N(Ynearest);
-                % Nearest training label with maximum frequency (if duplicated, check the nearest training instance)
-                [~,J] = max(frequencies);
-                Ypred(i) = Ynearest(J);
-            end
+%k-Nearest Neighbors (kNN)
+%
+% SYNTAX
+% 1. label = predict_knneighbors(X,Y,Xnew,k)
+%
+% DESCRIPTION
+% 1. Returns the estimated labels of one or multiple test instances.
+%
+% X is a M-by-N matrix, with M instances of N features. 
+% Y is a M-by-1 matrix, with respective M labels to each training instance. 
+% Xnew is a P-by-N matrix, with P instances of N features to be classified.
+% k is a scalar with the number of nearest neighbors selected.
+%
+% EXAMPLE
+% 1.
+%      load fisheriris
+%      X = meas;
+%      Y = species;
+%      Xnew = [min(meas);mean(meas);max(meas)];
+%      k = 5;
+%      label = predict_knneighbors(X,Y,Xnew,k)
+%      label = 
+%          'setosa'
+%          'versicolor'
+%          'virginica'
+%
+% David Alan de Oliveira Ferreira (http://lattes.cnpq.br/3863655668683045)
+% PhD student in Electrical Engineering from the Federal University of Amazonas
+% e-mail: ferreirad08@gmail.com
 
-            Ypred = obj.C(Ypred);
-        end
-        function [Xnearest,Ynearest,distances] = find(obj,Xnew)
-            % Euclidean distance between two points
-            A = repmat(Xnew,size(obj.X,1),1);
-            distances = sqrt(sum((A-obj.X).^2,2));
-            % Sort the distances in ascending order and check the k nearest training instances
-            [distances,I] = sort(distances);
-            Xnearest = obj.X(I(1:obj.k),:);
-            Ynearest = obj.C(obj.Y(I(1:obj.k)));
-            distances = distances(1:obj.k);
+properties
+    k = 5
+    C
+    X
+    Y
+end
+methods
+    function obj = kNNeighbors(k)
+        if nargin > 0
+            obj.k = k;
         end
     end
+    function obj = fit(obj,X,Y)
+        [obj.C,~,obj.Y] = unique(Y);
+        obj.X = X;
+    end
+    function Ypred = predict(obj,Xnew)
+        P = size(Xnew,1);
+        Ypred = zeros(P,1);
+        for i = 1:P
+            % Euclidean distance between two points
+            A = repmat(Xnew(i,:),size(obj.X,1),1);
+            distances = sqrt(sum((A-obj.X).^2,2));
+            % Sort the distances in ascending order and check the k nearest training labels
+            [~,I] = sort(distances);
+            Ynearest = obj.Y(I(1:obj.k));
+            % Frequencies of the k nearest training labels
+            N = histc(Ynearest,1:max(Ynearest));
+            frequencies = N(Ynearest);
+            % Nearest training label with maximum frequency (if duplicated, check the nearest training instance)
+            [~,J] = max(frequencies);
+            Ypred(i) = Ynearest(J);
+        end
+
+        Ypred = obj.C(Ypred);
+    end
+    function [Xnearest,Ynearest,distances] = find(obj,Xnew)
+        % Euclidean distance between two points
+        A = repmat(Xnew,size(obj.X,1),1);
+        distances = sqrt(sum((A-obj.X).^2,2));
+        % Sort the distances in ascending order and check the k nearest training instances
+        [distances,I] = sort(distances);
+        Xnearest = obj.X(I(1:obj.k),:);
+        Ynearest = obj.C(obj.Y(I(1:obj.k)));
+        distances = distances(1:obj.k);
+    end
+end
 end
