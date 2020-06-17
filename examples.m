fc00d3ba16273675cc9c78dd705ff12cb9d58ac9
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-06-17 16:46:11 -0400

    Update examples.m

--- examples.m
+++ examples.m
@@ -4,36 +4,42 @@ clc
 addpath('dscibox_src')
 % savepath
 
-disp('Fisher iris')
+disp('Examples of classification using the fisheriris.mat')
+
 load('fisheriris.mat')
 [X,Xnew,Y,Ynew] = dsb_utilities.data_sampling(meas,species,0.30,'stratified');
 
 k = 5; % k must be an integer, 5 is the default
-mk = dsb_predictors.kNNeighbors(k);
-mk = mk.fit(X,Y);
-label = mk.predict(Xnew);
-disp(['kNN: ' num2str(dsb_utilities.accuracy_score(Ynew,label))])
-% [Xnearest,Ynearest,distances] = mk.find(Xnew(2,:))
+mdl = dsb_predictors.kNNeighbors(k);
+mdl = mdl.fit(X,Y);
+Ypred = mdl.predict(Xnew);
+accuracy = dsb_utilities.accuracy_score(Ynew,Ypred);
+disp(['kNN: ' num2str(accuracy)])
+% [Xnearest,Ynearest,distances] = mdl.find(Xnew(2,:))
 
-mg = dsb_predictors.GaussianNB();
-mg = mg.fit(X,Y);
-label = mg.predict(Xnew);
-disp(['GNB: ' num2str(dsb_utilities.accuracy_score(Ynew,label))])
-% [labels,probabilities] = mg.find(Xnew(2,:))
+mdl = dsb_predictors.GaussianNB();
+mdl = mdl.fit(X,Y);
+Ypred = mdl.predict(Xnew);
+accuracy = dsb_utilities.accuracy_score(Ynew,Ypred);
+disp(['GNB: ' num2str(accuracy)])
+% [Ysorted,probabilities] = mdl.find(Xnew(2,:))
 
-mdt = dsb_predictors.DTree();
-mdt = mdt.fit(X,Y);
-label = mdt.predict(Xnew);
-disp(['DT: ' num2str(dsb_utilities.accuracy_score(Ynew,label))])
+mdl = dsb_predictors.DTree();
+mdl = mdl.fit(X,Y);
+Ypred = mdl.predict(Xnew);
+accuracy = dsb_utilities.accuracy_score(Ynew,Ypred);
+disp(['DT: ' num2str(accuracy)])
 
 %%
 
 disp(' ')
-disp('Golf dataset')
+disp('Examples of classification using the golf-dataset.mat')
+
 load('golf-dataset.mat')
 [X,Xnew,Y,Ynew] = dsb_utilities.data_sampling(predictors,target,0.30,'stratified');
 
-mdt2 = dsb_predictors.DTree();
-mdt2 = mdt2.fit(X,Y);
-label = mdt2.predict(Xnew);
-disp(['DT: ' num2str(dsb_utilities.accuracy_score(Ynew,label))])
+mdl = dsb_predictors.DTree();
+mdl = mdl.fit(X,Y);
+Ypred = mdl.predict(Xnew);
+accuracy = dsb_utilities.accuracy_score(Ynew,Ypred);
+disp(['DT: ' num2str(accuracy)])
