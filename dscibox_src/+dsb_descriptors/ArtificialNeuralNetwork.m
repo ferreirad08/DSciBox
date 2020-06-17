b30500958c6c2beee2f90e896a8b5617b44b0592
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-06-17 17:23:38 -0400

    Delete ArtificialNeuralNetwork.m

--- dscibox_src/+dsb_descriptors/ArtificialNeuralNetwork.m
+++
@@ -1,8 +0,0 @@
-function label = ArtificialNeuralNetwork(X,Y,Xnew)
-%Author: David Ferreira - Federal University of Amazonas
-%PhD student in Electrical Engineering
-%Contact: ferreirad08@gmail.com
-%
-%Artificial Neural Network (ANN)
-
-end
