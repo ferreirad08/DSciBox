f69fc22190a3e6d3d1f84bdc44395581457f6027
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-06-16 21:34:19 -0400

    Update InformationGain.m

--- dscibox_src/dsb_preprocessing/InformationGain.m
+++ dscibox_src/dsb_preprocessing/InformationGain.m
@@ -1,22 +1,22 @@
 classdef InformationGain
     properties
-        k
+        n_features
         gain
         indexes
     end
     methods
-        function obj = InformationGain(k)
+        function obj = InformationGain(n_features)
             if nargin > 0
-                obj.k = k;
+                obj.n_features = n_features;
             end
         end
         function obj = fit(obj,X,Y)
-            [n_samples,n_features] = size(X);
-            obj.gain = ones(1,n_features)*entropy(Y);
-            for i = 1:n_features
+            [m,n] = size(X);
+            obj.gain = ones(1,n)*entropy(Y);
+            for i = 1:n
                 [~,~,feature] = unique(X(:,i));
                 for j = 1:max(feature)
-                    p = histc(feature(feature==j),j)/n_samples;
+                    p = histc(feature(feature==j),j)/m;
                     obj.gain(i) = obj.gain(i) - p*entropy(Y(feature==j));
                 end
             end
@@ -24,7 +24,7 @@ classdef InformationGain
             [obj.gain,obj.indexes] = sort(obj.gain,'descend');
         end
         function Xt = feature_selection(obj,X)
-            Xt = X(:,obj.indexes(1:obj.k));
+            Xt = X(:,obj.indexes(1:obj.n_features));
         end
     end
 end
