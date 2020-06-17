b118ab75c0a163ab17bf8881c9902b5f61b00597
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-06-17 13:49:39 -0400

    Update entropy.m

--- dscibox_src/+dsb_utilities/entropy.m
+++ dscibox_src/+dsb_utilities/entropy.m
@@ -1,6 +1,27 @@
 function e = entropy(Y)
-    % Calculate the probabilities
-    p = histc(Y,unique(Y))/numel(Y);
-    % Calculate the entropy
-    e = -sum(p.*log2(p));
+%Information Entropy
+%
+% SYNTAX
+% 1. e = dsb_utilities.entropy(Y)
+%
+% DESCRIPTION
+% 1. Returns the entropy for a vector.
+%
+% Y is a vector with various elements.
+%
+% EXAMPLE
+% 1.
+%      Y = {'setosa';'versicolor';'virginica'};
+%      e = dsb_utilities.entropy(Y)
+%      e =
+%          1
+%
+% David Alan de Oliveira Ferreira (http://lattes.cnpq.br/3863655668683045)
+% PhD student in Electrical Engineering from the Federal University of Amazonas
+% e-mail: ferreirad08@gmail.com
+
+% Calculate the probabilities
+p = histc(Y,unique(Y))/numel(Y);
+% Calculate the entropy
+e = -sum(p.*log2(p));
 end
