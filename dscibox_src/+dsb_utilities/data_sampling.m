ca82f4ce089f4c1609ac7f3384e1d57bf3ad3bc5
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-06-17 13:58:14 -0400

    Update data_sampling.m

--- dscibox_src/+dsb_utilities/data_sampling.m
+++ dscibox_src/+dsb_utilities/data_sampling.m
@@ -1,28 +1,28 @@
 function [X,Xnew,Y,Ynew] = data_sampling(X,Y,p,type)
 %Simple or Stratified Random Sampling
 %
-%Author: David Ferreira - Federal University of Amazonas
-%PhD student in Electrical Engineering
-%Contact: ferreirad08@gmail.com
+% SYNTAX
+% 1. [X,Xnew,Y,Ynew] = dsb_utilities.data_sampling(X,Y,p) % 'simple' is the default
+% 1. [X,Xnew,Y,Ynew] = dsb_utilities.data_sampling(X,Y,p,'stratified')
 %
-%Syntax
-%1. [X,Xnew,Y,Ynew] = data_sampling(X,Y,p)
-%1. [X,Xnew,Y,Ynew] = data_sampling(X,Y,p,'stratified')
+% DESCRIPTION
+% 1. Returns the estimated labels of one or multiple test instances.
 %
-%Description 
-%1. Returns the estimated labels of one or multiple test instances.
+% X is a M-by-N matrix, with M instances of N features. 
+% Y is a M-by-1 matrix, with respective M labels to each training instance. 
+% p is a float number between 0.0 and 1.0 and represent the proportion of the dataset to include in the test split.
 %
-%X is a M-by-N matrix, with M instances of N features. 
-%Y is a M-by-1 matrix, with respective M labels to each training instance. 
-%p is a float number between 0.0 and 1.0 and represent the proportion of the dataset to include in the test split.
+% EXAMPLE
+% 1.
+%      load fisheriris
+%      X = meas;
+%      Y = species;
+%      p = 0.30;
+%      [X,Xnew,Y,Ynew] = dsb_utilities.data_sampling(X,Y,p,'stratified')
 %
-%Examples
-%1.
-%     load fisheriris
-%     X = meas;
-%     Y = species;
-%     p = 0.25;
-%     [X,Xnew,Y,Ynew] = data_sampling(X,Y,p)
+% David Alan de Oliveira Ferreira (http://lattes.cnpq.br/3863655668683045)
+% PhD student in Electrical Engineering from the Federal University of Amazonas
+% e-mail: ferreirad08@gmail.com
 
 if nargin > 3 && strcmp(type,'stratified')
     [~,~,Ynumerical] = unique(Y);
