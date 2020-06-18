b593eb0049a40c712bd14b063a72eacd681daa9d
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-06-17 20:39:49 -0400

    Add files via upload

---
+++ examples_two.m
@@ -0,0 +1,26 @@
+clear
+clc
+
+% Add library folders to the search path
+addpath('dscibox_src')
+% savepath
+
+%%
+
+fprintf('Examples using the Iris dataset.\n');
+
+load('fisheriris.mat')
+Xnew = mean(meas); % Creating a new instance from the mean values
+
+fprintf('\nIn the k-Nearest Neighbors, it is possible to obtain the values of the features,\n labels and distances of the k nearest instances to a new instance.\n');
+
+k = 5; % k must be an integer, 5 is the default
+mdl = dsb_predictors.kNNeighbors(k);
+mdl = mdl.fit(meas,species);
+[Xnearest,Ynearest,distances] = mdl.find(Xnew)
+
+fprintf('\nIn Gaussian Naive Bayes, it is possible to obtain the probabilities of each label\n in relation to a new instance.\n');
+
+mdl = dsb_predictors.GaussianNB();
+mdl = mdl.fit(meas,species);
+[Ysorted,probabilities] = mdl.find(Xnew)
