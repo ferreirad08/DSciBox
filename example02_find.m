c1de6bb134e39666f6e3626fdbaf3e6d85b60f95
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-06-18 02:31:08 -0400

    Update example02_find.m

--- example02_find.m
+++ example02_find.m
@@ -15,7 +15,8 @@ Xnew = mean(meas); % Creating a new instance from the mean values
 fprintf('\nIn the k-Nearest Neighbors, it is possible to obtain the values of the features,\n labels and distances of the k nearest instances to a new instance.\n');
 
 k = 5; % k must be an integer, 5 is the default
-mdl = dsb_predictors.kNNeighbors(k);
+p_norm = 2; % p_norm must be an integer, 2 is the default
+mdl = dsb_predictors.kNNeighbors(k,p_norm);
 mdl = mdl.fit(meas,species);
 [Xnearest,Ynearest,distances] = mdl.find(Xnew)
 
