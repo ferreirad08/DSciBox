b140b51e7dcdceaca5820fcbfbbc2cb1b9012bc6
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-06-23 02:22:03 -0400

    Update kMeans.m

--- dscibox_src/+dsb_predictors/kMeans.m
+++ dscibox_src/+dsb_predictors/kMeans.m
@@ -1,6 +1,19 @@
-function [C2,i] = kMeans(X,C)
+function [C2,i] = kMeans(X,arg2)
+if nargin < 2
+    arg2 = 8;
+end
+
+n_samples = size(X,1);
+if isscalar(arg2)
+    n_centroids = arg2;
+    C = X(randperm(n_samples,n_centroids),:);
+else
+    C = arg2;
+    n_centroids = size(C,1);
+end
+
 while 1
-    [C2,i] = kMeans2(X,C)
+    [C2,i] = kMeans2(X,C,n_samples,n_centroids)
     if C == C2
         break
     end
@@ -8,9 +21,7 @@ while 1
 end
 end
 
-function [C,i] = kMeans2(X,C)
-n_samples = size(X,1);
-n_centroids = size(C,1);
+function [C,i] = kMeans2(X,C,n_samples,n_centroids)
 distances = zeros(n_samples,n_centroids);
 for i = 1:n_centroids
     A = repmat(C(i,:),n_samples,1) - X;
