e45d025f506f276a7153bc4f3fe55c3facd7e929
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-06-24 09:57:33 -0400

    Create cdist.m

---
+++ dscibox_src/+dsb_utilities/cdist.m
@@ -0,0 +1,12 @@
+function D = cdist(X,Y,p)
+if nargin < 3
+    p = 2; % p-norm
+end
+n = size(X,1);
+k = size(Y,1);
+D = zeros(n,k);
+for i = 1:k
+    A = repmat(Y(i,:),n,1) - X;
+    D(:,i) = vecnorm(A,p,2);
+end
+end
