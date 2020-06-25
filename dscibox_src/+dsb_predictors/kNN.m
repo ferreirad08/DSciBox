9ccee601cf27044d1dd9f72fead9c73845c52ab5
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-06-24 23:48:44 -0400

    Update kNN.m

--- dscibox_src/+dsb_predictors/kNN.m
+++ dscibox_src/+dsb_predictors/kNN.m
@@ -7,7 +7,7 @@ D = dsb_utilities.cdist(Xnew,X);
 % select the k nearest instances
 I = I(:,1:k);
 Ynearest = Y(I);
-% count the frequencies of the labels
+% counts the frequencies of the labels
 frequencies = histc(Ynearest,1:max(Y),2);
 % select the most frequent label
 [~,J] = max(frequencies,[],2);
