4490f538626f2a9ce4b75980b2c9b4514cb21637
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-06-24 23:44:27 -0400

    Update kNN.m

--- dscibox_src/+dsb_predictors/kNN.m
+++ dscibox_src/+dsb_predictors/kNN.m
@@ -1,15 +1,15 @@
 function [Ypred,I] = kNN(X,Y,Xnew,k)
 [C,~,Y] = unique(Y);
-% compute the distances between the instances
+% calculate the distances between the instances
 D = dsb_utilities.cdist(Xnew,X);
 % sort the distances in ascending order
 [~,I] = sort(D,2);
 % select the k-nearest instances
 I = I(:,1:k);
 Ynearest = Y(I);
-% count the frequencies of labels
+% count label frequencies
 frequencies = histc(Ynearest,1:max(Y),2);
-% select the label with maximum frequency
+% select the most frequent label
 [~,J] = max(frequencies,[],2);
 Ypred = C(J);
 end
