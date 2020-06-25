21d936074c42d6f4e821482240a18f6aaa3f48cd
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-06-25 00:37:56 -0400

    Update kNN.m

--- dscibox_src/+dsb_predictors/kNN.m
+++ dscibox_src/+dsb_predictors/kNN.m
@@ -1,12 +1,9 @@
 function [Ypred,I] = kNN(X,Y,Xnew,k)
 [C,~,Y] = unique(Y);
-% Calculate the distance between any two instances
 D = dsb_utilities.cdist(Xnew,X);
-% Find the nearest instances
 [~,I] = sort(D,2);
 I = I(:,1:k);
 Ynearest = Y(I);
-% Majority vote on a class labels based on the nearest instances list
 frequencies = histc(Ynearest,1:max(Y),2);
 [~,J] = max(frequencies,[],2);
 Ypred = C(J);
