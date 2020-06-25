c0dfa735f9a7b54693cf6dc417c3a437729ec4f8
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-06-24 23:51:38 -0400

    Update kNN.m

--- dscibox_src/+dsb_predictors/kNN.m
+++ dscibox_src/+dsb_predictors/kNN.m
@@ -1,6 +1,6 @@
 function [Ypred,I] = kNN(X,Y,Xnew,k)
 [C,~,Y] = unique(Y);
-% calculate distances between instances
+% calculate the distances between the instances
 D = dsb_utilities.cdist(Xnew,X);
 % sorts the distances in ascending order
 [~,I] = sort(D,2);
