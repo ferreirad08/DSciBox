fd01b126db211e3fe71b0b6029506b35ef83fc24
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-07-17 08:45:56 -0400

    Update MinMaxNormalizer.m

--- dscibox_src/+dsb_preprocessing/MinMaxNormalizer.m
+++ dscibox_src/+dsb_preprocessing/MinMaxNormalizer.m
@@ -1,3 +1,29 @@
-function Xn = MinMaxNormalizer(X)
-Xn = (X-min(X))./(max(X)-min(X));
+classdef MinMaxNormalizer
+%Min-Max Normalizer
+%
+% David Alan de Oliveira Ferreira (http://lattes.cnpq.br/3863655668683045)
+% PhD student in Electrical Engineering from the Federal University of Amazonas
+% e-mail: ferreirad08@gmail.com
+
+properties
+    n
+    min_
+    max_
+end
+methods
+    function obj = MinMaxNormalizer(n)
+        if nargin > 0
+            obj.n = n;
+        end
+    end
+    function obj = fit(obj,X)
+        obj.min_ = min(X);
+        obj.max_ = max(X);
+    end
+    function Xn = transform(obj,X)
+        n_ = size(X,1);
+        Xn = (X-repmat(obj.min_,n_,1))...
+            ./(repmat(obj.max_,n_,1)-repmat(obj.min_,n_,1));
+    end
+end
 end
