c6bea75312f083f0c17d0f8bf0ca83583a1c2a48
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-07-22 11:56:16 -0400

    Update LinearRegression.m

--- dscibox_src/+dsb_predictors/LinearRegression.m
+++ dscibox_src/+dsb_predictors/LinearRegression.m
@@ -4,29 +4,31 @@ classdef LinearRegression
 % SYNTAX
 % 1. reg = dsb_predictors.LinearRegression()
 %    reg = reg.fit(X,Y)
-%    [Ypred,R_squared] = reg.predict(Xnew)
+%    Ypred = reg.predict(Xnew)
 %
 % DESCRIPTION
-% 1. Returns the estimated labels of one or multiple test instances.
-% 2. Returns the values of the features, labels and distances of the k
-% nearest instances to a new instance. 
+% 1. Returns the regression to new observations of an explanatory (independent) variable. 
 %
 % X is a vector with M observations of an explanatory (independent) variable.
 % Y is a vector with respective M observations of an explained (dependent)
 % variable.
-% R_squared is the Pearson correlation coefficient (rho) squared.
 %
 % EXAMPLE
 % 1.
 %      X = [2,3,9,6];
 %      Y = [5,7,12,8];
 %      reg = LinearRegression().fit(X,Y);
+%      reg.coeff
+%      ans =
+%          0.9000 3.5000
+%      reg.R_squared
+%      ans =
+%          0.9346
+%
 %      Ynew = [2,3,9,6];
-%      [Ypred,R_squared] = reg.predict(Xnew)
+%      Ypred = reg.predict(Xnew)
 %      Ypred =
 %          5.3000    6.2000   11.6000    8.9000
-%      R_squared =
-%          0.9346
 %
 % David Alan de Oliveira Ferreira (http://lattes.cnpq.br/3863655668683045)
 % PhD student in Electrical Engineering from the Federal University of
@@ -36,7 +38,7 @@ classdef LinearRegression
 properties
     n
     coeff
-    Y
+    R_squared
 end
 methods
     function obj = LinearRegression(n)
@@ -49,16 +51,16 @@ methods
         a = cov(X,Y)/var(X);
         b = mean(Y)-a*mean(X);
         obj.coeff = [a, b];
-        obj.Y = Y;
-    end
-    function [Ypred,R_squared] = predict(obj,Xnew)
-        % estimating points of the line
-        Ypred = obj.coeff(1)*Xnew+obj.coeff(2);
         
         % calculating the Pearson correlation coefficient (rho) and the
-        % determination coefficient (R^2) 
-        rho = cov(obj.Y,Ypred)/(std(obj.Y)*std(Ypred));
-        R_squared = rho*rho;
+        % determination coefficient (R^2)
+        Ypred = predict(obj,X);
+        rho = cov(Y,Ypred)/(std(Y)*std(Ypred));
+        obj.R_squared = rho*rho;
+    end
+    function Ypred = predict(obj,Xnew)
+        % estimating points of the line
+        Ypred = obj.coeff(1)*Xnew+obj.coeff(2);
     end
 end
 end
