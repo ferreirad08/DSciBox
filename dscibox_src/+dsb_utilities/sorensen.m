7eb6b59f27d7be640ec71f1d8d78b675c2890881
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-07-23 11:48:24 -0400

    Update sorensen.m

--- dscibox_src/+dsb_utilities/sorensen.m
+++ dscibox_src/+dsb_utilities/sorensen.m
@@ -1,3 +1,9 @@
-function D = sorensen(A,B)
-D = sum(abs(A-B),2) ./ sum(A+B,2);
+function D = sorensen(X,Y)
+n = size(X,1);
+k = size(Y,1);
+D = zeros(n,k);
+for i = 1:k
+    A = repmat(Y(i,:),n,1);
+    D(:,i) = sum(abs(A-X),2) ./ sum(A+X,2);
+end
 end
