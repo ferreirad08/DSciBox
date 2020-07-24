9e3b19f2120379e8714bb4e90117ac52c413bc95
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-07-24 13:32:13 -0400

    Update cross_validation.m

--- dscibox_src/+dsb_utilities/cross_validation.m
+++ dscibox_src/+dsb_utilities/cross_validation.m
@@ -25,8 +25,8 @@ n = numel(Y);
 if n < k
     error('Error occurred: the number of samples must not be less than k.')
 end
-n = n-rem(n,k);
-i = reshape(randperm(n),n/k,k);
+n2 = n-rem(n,k);
+i = reshape(randperm(n,n2),n2/k,k);
 
 accuracy = zeros(1,k);
 for fold = 1:k
