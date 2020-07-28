c8d444827b44e0ef54ef9e19055fe5925535e664
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-07-28 12:52:46 -0400

    Add files via upload

---
+++ dscibox_src/+dsb_classification/kNNeighbors.m
@@ -0,0 +1,71 @@
+classdef kNNeighbors
+%k-Nearest Neighbors (kNN)
+%
+% SYNTAX
+% 1. mdl = dsb_predictors.kNNeighbors(k,metric)
+%    mdl = mdl.fit(X,Y)
+%    Ypred = mdl.predict(Xnew)
+% 2. [indices,distances] = mdl.find(Xnew)
+%
+% DESCRIPTION
+% 1. Returns the estimated labels of one or multiple test instances.
+% 2. Returns the indices and distances of the k
+% nearest instances to a new instance. 
+%
+% X is a M-by-N matrix, with M instances of N features. 
+% Y is a M-by-1 matrix, with respective M labels to each training instance. 
+% Xnew is a P-by-N matrix, with P instances of N features to be classified.
+% k is a scalar with the number of nearest neighbors selected.
+% metric is the distance metric.
+%
+% David Alan de Oliveira Ferreira (http://lattes.cnpq.br/3863655668683045)
+% PhD student in Electrical Engineering from the Federal University of
+% Amazonas 
+% e-mail: ferreirad08@gmail.com
+
+properties
+    k = 5
+    metric = 'euclidean'
+    C
+    X
+    Y
+end
+methods
+    function obj = kNNeighbors(k,metric)
+        if nargin > 0
+            obj.k = k;
+        end
+        if nargin > 1
+            obj.metric = metric;
+        end
+    end
+    function obj = fit(obj,X,Y)
+        [obj.C,~,obj.Y] = unique(Y);
+        obj.X = X;
+    end
+    function Ypred = predict(obj,Xnew)
+        indices = find(obj,Xnew);
+        Ynearest = obj.Y(indices)';
+        if size(Xnew,1) == 1, Ynearest = Ynearest'; end
+        % Frequencies of the k nearest training labels
+        N = histc(Ynearest,1:max(obj.Y),1);
+        [n_class,P] = size(N);
+        frequencies = N(Ynearest...
+            + repmat(0:n_class:P*n_class-n_class,obj.k,1));
+        % Nearest training label with maximum frequency (if duplicated,
+        % check the nearest training instance) 
+        [~,J] = max(frequencies);
+        Ypred = Ynearest(J + (0:obj.k:P*obj.k-obj.k));
+        Ypred = obj.C(Ypred);
+    end
+    function [indices,distances] = find(obj,Xnew)
+        % Distance between two points
+        distances = dsb_utilities.cdist(Xnew,obj.X,obj.metric);
+        % Sort the distances in ascending order and check the k nearest
+        % training labels 
+        [distances,indices] = sort(distances,2);
+        distances = distances(:,1:obj.k);
+        indices = indices(:,1:obj.k);
+    end
+end
+end
---
+++ dscibox_src/+dsb_clustering/kMeans.m
@@ -0,0 +1,94 @@
+classdef kMeans
+%k-Means (kM)
+%
+% SYNTAX
+% 1. mdl = dsb_predictors.kMeans(arg1) % arg1 is the number of clusters or the centroids of each cluster
+%    mdl = mdl.fit(X)
+%    Ypred = mdl.predict(Xnew)
+%
+% DESCRIPTION
+% 1. Returns the estimated clusters of one or multiple test instances.
+%
+% k is a scalar with the number of clusters selected.
+% X is a M-by-N matrix, with M instances of N features.
+% Xnew is a P-by-N matrix, with P instances of N features for clustering.
+%
+% EXAMPLES
+% 1.
+% >> X = [[1, 2]; [1, 4]; [1, 0];[10, 2]; [10, 4]; [10, 0]];
+% >> k = 2;
+% >> mdl = dsb_descriptors.kMeans(k).fit(X);
+% >> Xnew = [[0, 0]; [12, 3]];
+% >> Ypred = mdl.predict(Xnew)
+% 
+% Ypred =
+% 
+%      1
+%      2
+% 
+% >> mdl.C % Centroids
+% 
+% ans =
+% 
+%      1     2
+%     10     2
+%
+% David Alan de Oliveira Ferreira (http://lattes.cnpq.br/3863655668683045)
+% PhD student in Electrical Engineering from the Federal University of Amazonas
+% e-mail: ferreirad08@gmail.com
+
+properties
+    k = 2 % number of clusters
+    C = [] % centroids of each cluster
+    idx
+end
+methods
+    function obj = kMeans(arg1)
+        if nargin > 0
+            if isscalar(arg1)
+                obj.k = arg1;
+            else
+                obj.C = arg1;
+                obj.k = size(arg1,1);
+            end
+        end
+    end
+    function obj = fit(obj,X)
+        % initializing clusters by the k-means++ algorithm
+        if isempty(obj.C)
+            Xcopy = X;
+            i = randi(size(Xcopy,1));
+            obj.C(1,:) = Xcopy(i,:);
+            Xcopy(i,:) = [];
+            for j = 2:obj.k
+                % distance measure
+                D = dsb_utilities.cdist(Xcopy,obj.C).^2;
+                [~,i] = max(min(D,[],2));
+                obj.C(j,:) = Xcopy(i,:);
+                Xcopy(i,:) = [];
+            end
+        end
+        
+        % convergence
+        Cnew = zeros(size(obj.C));
+        while 1
+            % associate each instance with the closest centroid
+            obj.idx = predict(obj,X);
+            % recalculate each centroid using all instances associated with it
+            for j = 1:obj.k
+                Cnew(j,:) = mean(X(obj.idx == j,:));
+            end
+            
+            % break, if no instances change of associated centroid
+            if obj.C == Cnew, break, end
+            obj.C = Cnew;
+        end
+    end
+    function Ypred = predict(obj,Xnew)
+        % distance measure
+        D = dsb_utilities.cdist(Xnew,obj.C);
+        % associate each instance with the closest centroid
+        [~,Ypred] = min(D,[],2);
+    end
+end
+end
