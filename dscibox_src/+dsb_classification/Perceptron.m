1f21e1233c328b2ab0ab9cb32d662770bea8620e
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-08-13 11:56:09 -0400

    Update Perceptron.m

--- dscibox_src/+dsb_classification/Perceptron.m
+++ dscibox_src/+dsb_classification/Perceptron.m
@@ -3,22 +3,23 @@ classdef Perceptron
 
 properties
     alpha = 0.01 % Learning Rate
+    n_iter = 2000
     w
     bias = 0
 end
 methods
-    function obj = Perceptron(alpha,n_features)
-        if nargin > 0
-            obj.alpha = alpha;
-        end
-        
+    function obj = Perceptron(alpha,n_features,n_iter)
+        obj.alpha = alpha;
+        obj.n_iter = n_iter;
         obj.w = 1-2.*rand(1,n_features);
     end
     function obj = fit(obj,X,Y)
+        cum_error = [];
         for i = 1:size(X,1)
             output = sum(X(i,:).*obj.w) + obj.bias;
             Ypred = output >= 0;
             error = Y(i) - Ypred;
+            cum_error = [cum_error,error];
             if error ~= 0
                 obj.w = obj.w + obj.alpha*error.*X(i,:);
             end
