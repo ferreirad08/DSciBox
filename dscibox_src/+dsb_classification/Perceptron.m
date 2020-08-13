22cb8866c9bf982570e72b9e42fa1cbff8d1f5c9
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-08-13 12:03:18 -0400

    Update Perceptron.m

--- dscibox_src/+dsb_classification/Perceptron.m
+++ dscibox_src/+dsb_classification/Perceptron.m
@@ -14,15 +14,18 @@ methods
         obj.w = 1-2.*rand(1,n_features);
     end
     function obj = fit(obj,X,Y)
-        cum_error = [];
-        for i = 1:size(X,1)
-            output = sum(X(i,:).*obj.w) + obj.bias;
-            Ypred = output >= 0;
-            error = Y(i) - Ypred;
-            cum_error = [cum_error,error];
-            if error ~= 0
-                obj.w = obj.w + obj.alpha*error.*X(i,:);
+        for j = 1:obj.n_iter
+            cum_error = [];
+            for i = 1:size(X,1)
+                output = sum(X(i,:).*obj.w) + obj.bias;
+                Ypred = output >= 0;
+                error = Y(i) - Ypred;
+                cum_error = [cum_error,error];
+                if error ~= 0
+                    obj.w = obj.w + obj.alpha*error.*X(i,:);
+                end
             end
+            if cum_error == 0, break, end
         end
     end
     function Ypred = predict(obj,Xnew)
