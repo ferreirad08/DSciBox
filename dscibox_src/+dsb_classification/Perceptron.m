101f68b073439d93eb6249d5732a5b145927b035
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-08-14 12:08:39 -0400

    Update Perceptron.m

--- dscibox_src/+dsb_classification/Perceptron.m
+++ dscibox_src/+dsb_classification/Perceptron.m
@@ -29,8 +29,8 @@ methods
         for j = 1:obj.n_epochs
             cum_error = 0;
             for i = 1:n
-                u = sum(X(i,:).*obj.w);
-                Ypred = u >= 0; % Loss Function
+                u = sum(X(i,:).*obj.w); % Activation Potential
+                Ypred = u >= 0; % Activation Function
                 if Ypred ~= Y(i)
                     obj.w = obj.w + obj.eta*(Y(i) - Ypred).*X(i,:);
                     cum_error = cum_error + 1;
@@ -42,8 +42,8 @@ methods
     function Ypred = predict(obj,Xnew)
         n = size(Xnew,1);
         Xnew = [repmat(obj.bias,n,1), Xnew];
-        u = sum(Xnew.*repmat(obj.w,n,1),2);
-        Ypred = u >= 0; % Loss Function
+        u = sum(Xnew.*repmat(obj.w,n,1),2); % Activation Potential
+        Ypred = u >= 0; % Activation Function
         Ypred = obj.C(Ypred+1);
     end
 end
