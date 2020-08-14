633d51ca272c10faf3ed80ac6593222f5c7a568c
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-08-14 11:30:33 -0400

    Update Perceptron.m

--- dscibox_src/+dsb_classification/Perceptron.m
+++ dscibox_src/+dsb_classification/Perceptron.m
@@ -6,7 +6,6 @@ properties
     n_epochs = 2000
     w
     bias = -1
-    threshold
     C
 end
 methods
@@ -24,17 +23,15 @@ methods
     function obj = fit(obj,X,Y)
         [obj.C,~,Y] = unique(Y); Y = Y-1;
         
-        obj.w = rand(1,size(X,2));
-        obj.threshold = obj.bias*rand;
+        obj.w = rand(1,size(X,2)+1);
 
         for j = 1:obj.n_epochs
             cum_error = 0;
             for i = 1:size(X,1)
-                output = sum(X(i,:).*obj.w) + obj.threshold;
+                output = sum([obj.bias, X(i,:)].*obj.w);
                 Ypred = output >= 0; % Loss Function
                 if Ypred ~= Y(i)
-                    obj.w = obj.w + obj.eta*(Y(i) - Ypred).*X(i,:);
-                    obj.threshold = obj.threshold + obj.eta*(Y(i) - Ypred)*obj.bias;
+                    obj.w = obj.w + obj.eta*(Y(i) - Ypred).*[obj.bias, X(i,:)];
                     cum_error = cum_error + 1;
                 end
             end
@@ -42,7 +39,7 @@ methods
         end
     end
     function Ypred = predict(obj,Xnew)
-        output = sum(Xnew.*repmat(obj.w,size(Xnew,1),1),2) + obj.threshold;
+        output = sum([repmat(obj.bias,size(Xnew,1),1), Xnew].*repmat(obj.w,size(Xnew,1),1),2);
         Ypred = output >= 0; % Loss Function
         Ypred = obj.C(Ypred+1);
     end
