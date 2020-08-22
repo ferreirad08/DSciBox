ee95ce816d16703bdc6795c65da2b36ae110ee7f
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-08-22 17:29:55 -0400

    Update sigmf.m

--- dscibox_src/+dsb_utils/sigmf.m
+++ dscibox_src/+dsb_utils/sigmf.m
@@ -1,14 +1,8 @@
-function y = normpdf(x,mu,sigma)
+function y = sigmf(x,params)
 if nargin < 2
-    mu = 0;
+    params = [1, 0];
 end
-if nargin < 3
-    sigma = 1;
-end
-y = 1./(sigma.*sqrt(2.*pi))...
-    .*exp(-1/2.*((x-mu)./sigma).^2);
-end
-
-function y = sigmoid(x)
-y = 1./(1+exp(-x));
+a = params(1);
+c = params(2);
+y = 1./(1+exp(-a.*(x-c)));
 end
