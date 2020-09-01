bc6c775c4fab4739bc561272d9326d66480d018c
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-09-01 11:53:00 -0400

    Update SVM.m

--- dscibox_src/+dsb_classification/SVM.m
+++ dscibox_src/+dsb_classification/SVM.m
@@ -1,27 +1,23 @@
 clear, clc, load fisheriris
 addpath('C:\Program Files\MATLAB\R2016a\toolbox\dscibox_src')
-y_train(1:50) = -1;
-y_train(51:100) = 1;
+y_train(1:50,1) = -1;
+y_train(51:100,1) = 1;
 [X,Xnew,Y,Ynew] = dsb_utils.data_sampling(meas(1:100,[1 3]),y_train,0.10,'stratified');
 
 eta = 0.0001; % Learning Rate
 n_epochs = 10000; % Number of Epochs
 [n,m] = size(X);
-w = zeros(n,m); % Synaptic Weights
+w = zeros(1,m); % Synaptic Weights
 
 for j = 1:n_epochs
-    lambda = 1/j; % Regularization Parameter
-    
-    y = sum(w.*X,2);
-    prod = y .* Y;
-    
+    lambda = 1/j; % Regularization Parameter    
     for i = 1:n
-        if prod(i) >= 1
-            cost = 0;
+        y = sum(w.*X(i,:));
+        prod = Y(i) * y;
+        if prod >= 1
             w = w - eta * (2 * lambda * w);
         else
-            cost = 1 - prod(i);
-            w = w + eta * (repmat(y_train(i) * X(i,:),n,1) - 2 * lambda * w);
+            w = w + eta * (Y(i) * X(i,:) - 2 * lambda * w);
         end
     end
 end
