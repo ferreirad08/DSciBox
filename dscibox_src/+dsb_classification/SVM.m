16754d5546a4c6de34f42f3137a6b2e058be9626
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-08-31 22:08:53 -0400

    Update SVM.m

--- dscibox_src/+dsb_classification/SVM.m
+++ dscibox_src/+dsb_classification/SVM.m
@@ -1,3 +1,8 @@
 load('fisheriris.mat')
 [X,Xnew,Y,Ynew] = data_sampling(meas(1:100,[1 3]),species(1:100),0.10,'stratified')
 plot(X(:,1),X(:,2),'o')
+
+w = zeros(size(X))
+
+epochs = 1
+alpha = 0.0001
