0a6fa1769764b1b526173cd86ff4d234ea6fcc69
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-09-18 14:08:19 -0400

    Add files via upload

---
+++ dscibox_src/+dsb_regression/kNNeighborsRegressor.m
@@ -0,0 +1,44 @@
+classdef kNNeighborsRegressor
+properties
+    k = 5
+    metric = 'euclidean' % distance metric
+    weights = 'uniform'  % weight function
+    X
+    Y
+end
+methods
+    function obj = kNNeighborsRegressor(k,metric,weights)
+        if nargin > 0
+            obj.k = k;
+        end
+        if nargin > 1
+            obj.metric = metric;
+        end
+        if nargin > 2
+            obj.weights = weights;
+        end
+    end
+    function obj = fit(obj,X,Y)
+        obj.Y = Y;
+        obj.X = X;
+    end
+    function Ypred = predict(obj,Xnew)
+        dist = dsb_utils.cdist(obj.X,Xnew,obj.metric); % distances
+        P = size(Xnew,1); % number of tests
+        Ypred = zeros(1,P);
+        if strcmp(obj.weights,'uniform')
+            [~,ind] = sort(dist);
+            for i = 1:P
+                Ypred(i) = mean(obj.Y(ind(1:obj.k,i)));
+            end
+        elseif strcmp(obj.weights,'distance')
+            [dist,ind] = sort(dist);
+            w = 1./dist; % weights
+            for i = 1:P
+                Ypred(i) = sum(obj.Y(ind(1:obj.k,i)).*w(1:obj.k,i)')...
+                    /sum(w(1:obj.k,i));
+            end
+        end
+    end
+end
+end
