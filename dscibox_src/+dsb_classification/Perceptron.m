0e3c2647f74f6ce9807891f6efb4433ace4a10c6
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-11-03 12:33:05 -0400

    Update Perceptron.m

--- dscibox_src/+dsb_classification/Perceptron.m
+++ dscibox_src/+dsb_classification/Perceptron.m
@@ -1,9 +1,9 @@
 classdef Perceptron
 %Perceptron Network
 %
-% David Alan de Oliveira Ferreira (http://lattes.cnpq.br/3863655668683045)
-% D.Sc. student in Electrical Engineering from the Federal University of Amazonas
-% e-mail: ferreirad08@gmail.com
+% Author: M.Sc. David Ferreira - Federal University of Amazonas
+% Contact: ferreirad08@gmail.com
+% Date: August 2020
 
 properties
     eta = 0.01 % Learning Rate
